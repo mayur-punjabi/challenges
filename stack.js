7f7044b6abbf2a8be027342a63270a89c8703ee5
mayur punjabi <mayurpunjabi9@gmail.com>
2017-07-17 17:29:29 +0530

    stack updated

--- stack.js
+++ stack.js
@@ -1,52 +1,90 @@
-var Rectangle = [];
-
-var w=window.innerWidth;
-function setup(){
-  createCanvas(w,600);
-  for(var j=0;j<5;j++){
-    Rectangle[j]={
-    y: 200,
-    col: 0,
-    display: function(){
-      noStroke();
-      fill(0);
-      rect(((w/2)-40),this.y,80,20);
-    },
-    move: function(){
-      map(this.col,0,255,200,470);
-      if(this.y<470){
-        fill(this.col);
+var j=0;
+var k=0;
+var Rectangles = [];
+var i=0;
+var w=window.innerWidth-20;
+function Rectangle(){
+    this.y= 200;
+    this.col= 0;
+    this.i=i;
+    this.display= function(){
+        noStroke();
+        fill(0);
         rect(((w/2)-40),this.y,80,20);
-        this.y++;
-        this.col++;
-        console.log("rectangle[0] = "  + Rectangle[0].y);
-        console.log("rectangle[1] = "  + Rectangle[1].y);
-        console.log("rectangle[2] = "  + Rectangle[2].y);
-        console.log("rectangle[3] = "  + Rectangle[3].y);
-        console.log("rectangle[4] = "  + Rectangle[4].y);
-      }
-      if(this.y>=470){
-        this.y=470;
-        this.col=255;
-        fill(this.col);
-        rect(((w/2)-40),this.y,80,20);
-      }
     }
-  }
-}
-}
+    this.move= function(){
+        map(this.col,0,255-this.i,200,this.i);
+        if((this.y+(30*(this.i)))<470){
+            fill(this.col);
+            rect(((w/2)-40),this.y,80,20);
+            this.y++;
+            this.col++;
+        }
+        if((this.y+(30*(this.i)))>=470){
+            this.y=470-(30*(this.i));
+            this.col=255;
+            fill(this.col);
+            rect(((w/2)-40),this.y,80,20);
 
+        }
+    }
+    this.moveUp= function(){
+        map(this.col,255-this.i,0,200,this.i);
+        fill(this.col);
+        if(this.y>200){
+            rect(((w/2)-40),this.y,80,20);
+            this.y--;
+            this.col--;
+        }
+        if(this.y=200){
+            this.y=200;
+            this.col=0;
+            fill(this.col);
+            rect(((w/2)-40),this.y,80,20);
+        }
+    }
+}
+function setup(){
+    createCanvas(w,600);
+
+}
 function draw(){
-  background(0);
-  stroke(255);
-  line(((w/2)-50),500,((w/2)+50),500);
-  line(((w/2)-50),500,((w/2)-50),250);
-  line(((w/2)+50),500,((w/2)+50),250);
-  noStroke();
-  //fill(0);
-  Rectangle[0].display();
-  Rectangle[0].move();
-  Rectangle[1].display();
-  Rectangle[1].move();
-  console.log(Rectangle[0].y);
-  }
+    background(0);
+    stroke(255);
+    line(((w/2)-50),500,((w/2)+50),500);
+    line(((w/2)-50),500,((w/2)-50),250);
+    line(((w/2)+50),500,((w/2)+50),250);
+    noStroke();
+    for(k=0;k<Rectangles.length;k++){
+        Rectangles[k].display();
+        Rectangles[k].move();
+    }
+ }
+function pushElement() {
+    Rectangles.push(new Rectangle());
+    i++;
+    if(Rectangles.length>7){
+        Rectangles.length=8;
+    }
+    if(i>8){
+        alert("stack-overflow");
+        i=8;
+    }
+    console.log("i = "+i);
+    console.log("length = "+Rectangles.length);
+}
+function popElement(){
+    Rectangles[i].moveUp();//Uncaught TypeError: Cannot read property 'moveUp' of undefined
+    i--;
+
+    Rectangles.splice(-1,1);
+    if(Rectangles.length<=0){
+        Rectangles.length=0;
+    }
+    if(i<0){
+        alert("stack-underflow");
+        i=0;
+    }
+    console.log("i = "+i);
+    console.log("length = "+Rectangles.length);
+}
